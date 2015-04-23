cc6ddc6cc1b084a6660204af9204127010fff991
Rick Yoesting <rick.yoesting@gmail.com>
2015-04-23 01:21:20 -0500

    getting started

---
+++ index.js
@@ -0,0 +1,73 @@
+(function() {
+  'use strict';
+  var needle = require('needle');
+
+  var WebHookReporter = function(options) {
+    options                   = options                    || {};
+    options.url               = options.url                || null;
+    options.notify            = options.notify             || {};
+    options.notifySpecSuccess = options.notify.specSuccess || false;
+    options.notifySpecFailure = options.notify.specFailure || true;
+    options.notifySpecSkipped = options.notify.specSkipped || false;
+    options.notifyRunComplete = options.notify.runComplete || true;
+    options.notifyRunStart    = options.notify.runStart    || true;
+
+    this.onRunStart = function() {
+      if (options.url === null) {
+        write('WARN: karma-webhook-reporter: option.url is not set. No webhook will be called.');
+      } else if (options.notifyRunStart) {
+        postData(options.url, 'runStart', { });
+      }
+    };
+
+    this.onSpecComplete = function(browser, result) {
+      var type;
+
+      if (result.skipped) {
+        type = options.notifySpecSkipped ? 'specSkipped' : null;
+      } else if (result.success) {
+        type = options.notifySpecSuccess ? 'specSuccess' : null;
+      } else {
+        console.log('failed')
+        type = options.notifySpecFailure ? 'specFailure' : null;
+      }
+
+      if (type !== null) {
+        postData(options.url, type, { browser: browser.name, result: result });
+      }
+    };
+
+    this.onRunComplete = function(browsers, results) {
+      if (options.notifyRunComplete) {
+        postData(options.url, 'runComplete', { browsers: browsers, results: results });
+      }
+    };
+  };
+
+  WebHookReporter.$inject = ['config.webHookReporter'];
+
+  module.exports = { 'reporter:web-hook': ['type', WebHookReporter] };
+
+  function write(msg) {
+    process.stdout.write(msg + '\n');
+  };
+
+  function postData(url, type, data) {
+    if (!url) { return; }
+
+    var payload = {
+        type: type,
+        data: data
+      }, opts = {
+        json: true
+      };
+
+    needle.post(url, payload, opts, function(err, resp) {
+      if (err) {
+        console.log(err);
+      } else if (resp.statusCode < 200 || resp.statusCode > 299) {
+        console.log('received http status: ' + resp.statusCode)
+      }
+    });
+  };
+})();
